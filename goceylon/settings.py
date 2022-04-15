5e2fb4038ca84cb3067b49a35e88aab2bcc62ab3
supun-kalhara <gl.supunkalhara@gmail.com>
2022-04-16 04:37:55 +0530

    fixed allowed host

--- goceylon/settings.py
+++ goceylon/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'a$+jxf9twh)7!ghj*jtfe3iar5b^cfzmmw_)t0jfn8d7$-qg&b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1:8000', 'goceylon.herokuapp.com/', 'goceylon.lk']
+ALLOWED_HOSTS = ['127.0.0.1:8000', 'goceylon.herokuapp.com', 'goceylon.lk']
 
 
 # Application definition
