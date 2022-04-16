a66182300facee5fc07e6b0e821b0f3f18e5991c
supun-kalhara <gl.supunkalhara@gmail.com>
2022-04-16 17:03:58 +0530

    hosting test 6

--- goceylon/settings.py
+++ goceylon/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = ['127.0.0.1', 'goceylon.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
-    "whitenoise.runserver_nostatic",
+    #"whitenoise.runserver_nostatic",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
