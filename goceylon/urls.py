dc337382648a1a7dd71444de037003ca9bbd996a
supun-kalhara <gl.supunkalhara@gmail.com>
2022-04-24 00:41:56 +0530

    fixed upload button and testing ci

--- goceylon/urls.py
+++ goceylon/urls.py
@@ -53,9 +53,9 @@ urlpatterns = [
     path('recommendation/', recommendation_view, name='recommendation-view'),
 
 ]
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 
 # url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
 # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
